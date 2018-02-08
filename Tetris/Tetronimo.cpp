1f9011353b1f4285659989d938b461dffae6b432
John Na <jna0108@gmail.com>
2018-02-07 23:36:43 -0500

    set up basic SDL window

---
+++ Tetris/Tetronimo.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Tetronimo.h"
+
+
+Tetronimo::Tetronimo()
+{
+}
+
+
+Tetronimo::~Tetronimo()
+{
+}
---
+++ Tetris/Tetronimo.h
@@ -0,0 +1,8 @@
+#pragma once
+class Tetronimo
+{
+public:
+	Tetronimo();
+	virtual ~Tetronimo();
+};
+
