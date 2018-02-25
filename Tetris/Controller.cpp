e63bc15d3199c0e78dbd61961e1499ca496ed795
John Na <jna0108@gmail.com>
2018-02-25 02:18:33 -0500

    finish mino generation

--- Tetris/Controller.cpp
+++ Tetris/Controller.cpp
@@ -16,11 +16,24 @@ void Controller::draw(SDL_Renderer* renderer)
 
 void Controller::generatePiece()
 {
+	// Make next piece the current piece
+	cMino = nMino;
+	cRot = nRot;
+	cX = 0;
+	cY = 0;
 
+	// Spawn next piece
+	nMino = rand() % 7;
 }
 
 void Controller::initGame()
 {
+	// Spawn initial piece
+	cMino = rand() % 7; // 0 to 6
+	cRot = 0, cX = 0, cY = 0;
+
+	// Spawn initial next piece
+	nMino = rand() % 7;
 }
 
 void Controller::drawBoard(SDL_Renderer* renderer)
@@ -30,11 +43,11 @@ void Controller::drawBoard(SDL_Renderer* renderer)
 
 void Controller::drawMino(SDL_Renderer* renderer, int mino, int rotation, int x, int y)
 {
-	char pixel;
+	/*char pixel;
 	for (int i = 0; i < MINO_DIM; i++) {
 		for (int j = 0; j < MINO_DIM; j++) {
 			pixel = minoes->getMino(mino, rotation, i, j);
 			//boxColor(surface, -, -, -, -, RED); still need to calculate
 		}
-	}
+	}*/
 }
--- Tetris/Controller.h
+++ Tetris/Controller.h
@@ -23,11 +23,11 @@ public:
 private:
 	Board *board;
 	Tetromino *minoes;
-	int cMino, cRot, cX, cY; // current piece
-	int nMino, nRot, nX, nY; // next piece
+	int cMino, cRot, cX, cY;		// current piece
+	int nMino;						// next piece
+	const int nRot = 0, nX = 0, nY = 0;	// next piece's fields are always the same
 
 	void initGame();
 	void drawBoard(SDL_Renderer* renderer);
 	void drawMino(SDL_Renderer* renderer, int mino, int rotation, int x, int y);
 };
-
